838aa33b1fdfb447197d19fc370352d4193a24f1
darlingson <dmakuwila@gmail.com>
2023-12-19 03:48:34 +0200

    delete all files created for connecting to supabase

--- app/src/main/java/com/codeshinobi/zochitika/ManageProductApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.codeshinobi.zochitika
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-// ManageProductApplication.kt
-@HiltAndroidApp
-class ManageProductApplication: Application()
--- app/src/main/java/com/codeshinobi/zochitika/Product.kt
+++
@@ -1,8 +0,0 @@
-package com.codeshinobi.zochitika
-
-data class Product(
-    val id: String,
-    val name: String,
-    val price: Double,
-    val image: String?
-)
