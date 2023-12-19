bc61efd38ce74b76be6c5f490793ca21b28b694c
darlingson <dmakuwila@gmail.com>
2023-12-19 04:45:44 +0200

    purged supabase code from MainActivity

--- app/src/main/java/com/codeshinobi/zochitika/MainActivity.kt
+++ app/src/main/java/com/codeshinobi/zochitika/MainActivity.kt
@@ -3,69 +3,24 @@ package com.codeshinobi.zochitika
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.codeshinobi.zochitika.ui.theme.ZochitikaTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-import com.supabase.SupabaseClient
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var supabaseClient: SupabaseClient
-
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ZochitikaTheme {
-                // A surface container using the 'background' color from the theme
-                val navController = rememberNavController()
-                val currentBackStack by navController.currentBackStackEntryAsState()
-                val currentDestination = currentBackStack?.destination
+                // A surface container using the 'background' color fr
                 Scaffold { innerPadding ->
-                    NavHost(
-                        navController,
-                        startDestination = ProductListDestination.route,
-                        Modifier.padding(innerPadding)
-                    ) {
-                        composable(ProductListDestination.route) {
-                            ProductListScreen(
-                                navController = navController
-                            )
-                        }
-
-                        composable(AddProductDestination.route) {
-                            AddProductScreen(
-                                navController = navController
-                            )
-                        }
-
-                        composable(
-                            route = "${ProductDetailsDestination.route}/{${ProductDetailsDestination.productId}}",
-                            arguments = ProductDetailsDestination.arguments
-                        ) { navBackStackEntry ->
-                            val productId =
-                                navBackStackEntry.arguments?.getString(ProductDetailsDestination.productId)
-                            ProductDetailsScreen(
-                                productId = productId,
-                                navController = navController,
-                            )
-                        }
-                    }
+                    Text(text = "Hi there!", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
