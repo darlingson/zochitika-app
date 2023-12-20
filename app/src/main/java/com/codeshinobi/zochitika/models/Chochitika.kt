d62e3ac0542374bdca2b6b312b10a7fa7e3caa02
darlingson <dmakuwila@gmail.com>
2023-12-20 21:37:28 +0200

    created Chochitika model

---
+++ app/src/main/java/com/codeshinobi/zochitika/models/Chochitika.kt
@@ -0,0 +1,20 @@
+package com.codeshinobi.zochitika.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Chochitika(
+    val address:String?,
+    val coordinates:String,
+    val date:String,
+    val description:String,
+    val end_date:String,
+    val end_time:String,
+    val entry_fee:String,
+    val id:Int,
+    val location:String,
+    val organiser:String,
+    val time:String,
+    val title:String,
+    val type:String
+)
