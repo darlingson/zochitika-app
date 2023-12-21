d4c0255e5b107db68a755927bc01e7d243b1b193
darlingson <dmakuwila@gmail.com>
2023-12-21 15:24:46 +0200

    added cards to display the events

--- app/src/main/java/com/codeshinobi/zochitika/MainActivity.kt
+++ app/src/main/java/com/codeshinobi/zochitika/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -20,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codeshinobi.zochitika.models.Chochitika
 import com.codeshinobi.zochitika.ui.theme.ZochitikaTheme
 import kotlinx.coroutines.Dispatchers
@@ -27,6 +33,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import java.io.FileNotFoundException
 import java.net.URL
 
 class MainActivity : ComponentActivity() {
@@ -36,48 +43,81 @@ class MainActivity : ComponentActivity() {
         setContent {
             ZochitikaTheme {
                 // A surface container using the 'background' color fr
-                Scaffold { innerPadding ->
-                    Column(
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        Text(text = "Hi there!", modifier = Modifier.padding(innerPadding))
-                        DisplayDataFromEndpoint()
-                    }
-                }
+                MainScreen()
             }
         }
     }
 }
+
 @Composable
 fun DisplayDataFromEndpoint() {
     val coroutineScope = rememberCoroutineScope()
-//    var data by remember { mutableStateOf<List<Post>?>(null) }
     var data by remember { mutableStateOf<List<Chochitika>?>(null) }
-    val context:Context = LocalContext.current
+    val context: Context = LocalContext.current
     LaunchedEffect(Unit) {
         coroutineScope.launch {
             data = fetchDataFromEndpoint(context)
         }
     }
 
-    Column {
-        data?.forEach { post ->
-            post.title?.let { Text(text = it) }
-        } ?: Text(text = "Loading...")
+    LazyColumn {
+        data?.let {
+            items(it.size) { item ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = it[item].title ?: "No Title")
+                        Text(text = it[item].date ?: "No Date")
+                    }
+                }
+            }
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Zochitika App")
+                },
+                actions = {
+                    // Add any actions here
+                }
+            )
+        },
+        content = {
+            Text(text = "Zochitila",Modifier.padding(it))
+            DisplayDataFromEndpoint()
+        }
+    )
 }
 
 private val json = Json { ignoreUnknownKeys = true }
 
-suspend fun fetchDataFromEndpoint(context:Context): List<Chochitika> {
+suspend fun fetchDataFromEndpoint(context: Context): List<Chochitika> {
     return withContext(Dispatchers.IO) {
         try {
             val response = URL("https://darlingson.pythonanywhere.com/zochitika").readText()
             Log.d("EndpointResponse", response) // Log the response
             json.decodeFromString<List<Chochitika>>(response)
+        } catch (e: FileNotFoundException) {
+            Log.e("FetchDataError", "File not found at the specified URL", e)
+            emptyList() // Return an empty list in case of a 404 error
         } catch (e: Exception) {
             Log.e("FetchDataError", "Error fetching data from endpoint", e)
-            emptyList() // Return an empty list in case of an error
+            emptyList() // Return an empty list for other errors
         }
     }
 }
