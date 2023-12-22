b4782e5c7154d40fec72d86864a88b90045731f7
darlingson <dmakuwila@gmail.com>
2023-12-22 09:50:07 +0200

    added code to show event details

--- app/src/main/java/com/codeshinobi/zochitika/screens/ChochitikaInfo.kt
+++ app/src/main/java/com/codeshinobi/zochitika/screens/ChochitikaInfo.kt
@@ -1,14 +1,14 @@
 package com.codeshinobi.zochitika.screens
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
@@ -19,22 +19,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.codeshinobi.zochitika.screens.ui.theme.ZochitikaTheme
+
 
 class ChochitikaInfo : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            intent.getStringExtra("text")
             ZochitikaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ChochitikaInfoMain("Android")
+                    ChochitikaInfoMain("Android",intent = intent)
                 }
             }
         }
@@ -42,15 +43,45 @@ class ChochitikaInfo : ComponentActivity() {
 }
 
 @Composable
-fun ChochitikaInfoMain(name: String, modifier: Modifier = Modifier) {
+fun ChochitikaInfoMain(name: String, modifier: Modifier = Modifier, intent: Intent) {
     Column {
         Text(
             text = "Hello $name!",
             modifier = modifier
-                .fillMaxWidth())
+                .fillMaxWidth()
+        )
+
         imageFromURL()
+        EventInfo(intent)
+
     }
 
+}
+@Composable
+fun EventInfo(intent: Intent) {
+    var title :String = intent.getStringExtra("title").toString()
+    var organiser :String = intent.getStringExtra("organiser").toString()
+    var date :String = intent.getStringExtra("date").toString()
+    var address:String = intent.getStringExtra("address").toString()
+    var coordinates:String = intent.getStringExtra("coordinates").toString()
+    var description:String = intent.getStringExtra("description").toString()
+    var end_date:String = intent.getStringExtra("end_date").toString()
+    var end_time:String = intent.getStringExtra("end_time").toString()
+    var entry_fee:String = intent.getStringExtra("entry_fee").toString()
+    var location:String = intent.getStringExtra("location").toString()
+    var time:String = intent.getStringExtra("time").toString()
+    var type:String = intent.getStringExtra("type").toString()
+    var id:String = intent.getStringExtra("id").toString()
+    Column {
+        Text(text = title)
+        Text(text = "by : ${organiser}")
+        Text(text = "at : ${location}")
+        Text(text = "on : ${date}")
+        Text(text = "Start Time : ${time}")
+        Text(text = "address : ${address}")
+
+
+    }
 }
 @Composable
 fun imageFromURL() {
@@ -59,22 +90,22 @@ fun imageFromURL() {
         // in this column we are adding modifier
         // to fill max size, mz height and max width
         modifier = Modifier
-            .fillMaxSize()
-            .fillMaxHeight()
+//            .fillMaxSize()
+            .height(200.dp)
             .fillMaxWidth()
             // on below line we are adding
             // padding from all sides.
             .padding(10.dp),
         // on below line we are adding vertical
         // and horizontal arrangement.
-        verticalArrangement = Arrangement.Center,
+//        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // on below line we are adding image for our image view.
         Image(
             // on below line we are adding the image url
             // from which we will  be loading our image.
-            painter = rememberAsyncImagePainter("https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png"),
+            painter = rememberAsyncImagePainter("https://picsum.photos/600?blur=2"),
 
             // on below line we are adding content
             // description for our image.
@@ -89,10 +120,10 @@ fun imageFromURL() {
         )
     }
 }
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZochitikaTheme {
-        ChochitikaInfoMain("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ZochitikaTheme {
+//        ChochitikaInfoMain("Android",intent = intent)
+//    }
+//}
