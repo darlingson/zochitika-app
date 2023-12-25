92e10b4972fe1e6e82770322ef995b0283351b60
darlingson <dmakuwila@gmail.com>
2023-12-25 10:31:15 +0200

    added the splashActivity

--- app/src/main/java/com/codeshinobi/zochitika/SplashActivity.kt
+++ app/src/main/java/com/codeshinobi/zochitika/SplashActivity.kt
@@ -3,44 +3,38 @@ package com.codeshinobi.zochitika
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.codeshinobi.zochitika.ui.theme.ZochitikaTheme
+import com.codeshinobi.zochitika.viewmodels.SplashViewModel
 
 class SplashActivity : ComponentActivity() {
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition{viewModel.isLoading.value}
+
         setContent {
             ZochitikaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.surface) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(text = "Main Activity")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZochitikaTheme {
-        Greeting("Android")
     }
 }
