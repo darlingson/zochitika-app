594fda73276dddee8ef6200eedb157859b97b011
darlingson <dmakuwila@gmail.com>
2023-12-28 15:07:11 +0200

    added textfields for the name and location

--- app/src/main/java/com/codeshinobi/zochitika/screens/navigation/Profile.kt
+++ app/src/main/java/com/codeshinobi/zochitika/screens/navigation/Profile.kt
@@ -1,25 +1,24 @@
 package com.codeshinobi.zochitika.screens.navigation
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.codeshinobi.zochitika.R
 import com.codeshinobi.zochitika.ui.theme.ZochitikaTheme
 
 @Composable
@@ -30,27 +29,45 @@ fun ProfileScreen(navController: NavController) {
             color = MaterialTheme.colorScheme.background
         ) {
             Column(
-                modifier = Modifier.fillMaxSize().padding(15.dp),
+                modifier = Modifier
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
             )  {
-                Box(
-                    modifier = Modifier.fillMaxWidth()
-                        .height(200.dp)
-                        .padding(horizontal = 15.dp, vertical = 10.dp)
-                        .clip(MaterialTheme.shapes.large)
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.baseline_calendar_month_24),
-                        contentDescription = "profile_screen_bg",
-                        contentScale = ContentScale.Crop
-                    )
+                var name by rememberSaveable { mutableStateOf("") }
+                var loc by rememberSaveable {
+                    mutableStateOf("")
                 }
-                Text(
-                    "Profile Screen",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(vertical = 20.dp)
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 15.dp)
+                ) {
+                    TextField(
+                        value = name,
+                        onValueChange = {
+                            name = it
+                        },
+                        Modifier.fillMaxWidth(),
+                        label = { Text("Name") }
+                    )
+                    TextField(
+                        value = loc,
+                        onValueChange = {
+                            loc = it
+                        },
+                        Modifier.fillMaxWidth(),
+                        label = { Text("Location") }
+                    )
+                    Button(
+                        onClick = { /*TODO*/ },
+                        Modifier.fillMaxWidth(),
+                    ) {
+                        Text("Save")
+                    }
+                    Spacer(modifier = Modifier.padding(top=16.dp, bottom = 16.dp))
+                    Text(text = "Current Info")
+
+                }
             }
         }
     }
