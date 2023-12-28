100b052ff8c345ce4cdc3c17ae4aaa90c36522c2
darlingson <dmakuwila@gmail.com>
2023-12-28 13:36:29 +0200

    created bottom navigation items

---
+++ app/src/main/java/com/codeshinobi/zochitika/screens/navigation/NavigationItems.kt
@@ -0,0 +1,35 @@
+package com.codeshinobi.zochitika.screens.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigationItem(
+    val label : String = "",
+    val icon : ImageVector = Icons.Filled.Home,
+    val route : String = ""
+) {
+
+    //function to get the list of bottomNavigationItems
+    fun bottomNavigationItems() : List<BottomNavigationItem> {
+        return listOf(
+            BottomNavigationItem(
+                label = "Home",
+                icon = Icons.Filled.Home,
+                route = Screens.Home.route
+            ),
+            BottomNavigationItem(
+                label = "Near me",
+                icon = Icons.Filled.FavoriteBorder,
+                route = Screens.Near.route
+            ),
+            BottomNavigationItem(
+                label = "Profile",
+                icon = Icons.Filled.AccountCircle,
+                route = Screens.Profile.route
+            ),
+        )
+    }
+}
--- app/src/main/java/com/codeshinobi/zochitika/screens/navigation/routes.kt
+++ app/src/main/java/com/codeshinobi/zochitika/screens/navigation/routes.kt
@@ -2,6 +2,6 @@ package com.codeshinobi.zochitika.screens.navigation
 
 sealed class Screens(val route : String) {
     object Home : Screens("home_route")
-    object Search : Screens("search_route")
+    object Near : Screens("near_route")
     object Profile : Screens("profile_route")
 }
