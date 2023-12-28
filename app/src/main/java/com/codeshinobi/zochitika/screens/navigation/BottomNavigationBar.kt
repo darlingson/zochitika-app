c2f7dcc2af9c51260954b91d47fddcb8a89ae220
darlingson <dmakuwila@gmail.com>
2023-12-28 13:51:59 +0200

    added BottomNavigationBar composable

---
+++ app/src/main/java/com/codeshinobi/zochitika/screens/navigation/BottomNavigationBar.kt
@@ -0,0 +1,75 @@
+package com.codeshinobi.zochitika.screens.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavigationBar() {
+    var navigationSelectedItem by remember {
+        mutableStateOf(0)
+    }
+    val navController = rememberNavController()
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                BottomNavigationItem().bottomNavigationItems().forEachIndexed {index,navigationItem ->
+                    NavigationBarItem(
+                        selected = index == navigationSelectedItem,
+                        label = {
+                            Text(navigationItem.label)
+                        },
+                        icon = {
+                            Icon(
+                                navigationItem.icon,
+                                contentDescription = navigationItem.label
+                            )
+                        },
+                        onClick = {
+                            navigationSelectedItem = index
+                            navController.navigate(navigationItem.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) {paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screens.Home.route,
+            modifier = Modifier.padding(paddingValues = paddingValues)) {
+            composable(Screens.Home.route) {
+                //call our composable screens here
+            }
+            composable(Screens.Near.route) {
+                //call our composable screens here
+            }
+            composable(Screens.Profile.route) {
+                //call our composable screens here
+            }
+        }
+    }
+}
