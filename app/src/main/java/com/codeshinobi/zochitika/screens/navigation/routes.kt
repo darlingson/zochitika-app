caf935354c11c2f13c01ce2536cd69f1f692e8e1
darlingson <dmakuwila@gmail.com>
2023-12-28 13:32:19 +0200

    added routes class

---
+++ app/src/main/java/com/codeshinobi/zochitika/screens/navigation/routes.kt
@@ -0,0 +1,7 @@
+package com.codeshinobi.zochitika.screens.navigation
+
+sealed class Screens(val route : String) {
+    object Home : Screens("home_route")
+    object Search : Screens("search_route")
+    object Profile : Screens("profile_route")
+}
